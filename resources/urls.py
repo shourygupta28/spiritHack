e6bcdec2a75a7e980ae5d372ac6f0e85b5ea7be2
N15H4N7 <ngupta_be19@thapar.edu>
2021-03-20 22:17:39 +0530

    Bhut saare changes

--- resources/urls.py
+++ resources/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('resources/',								all_subs,			name='resources'),
+    path('resources/',								all_subs,				name='resources'),
+    path('resources/add',							create_resource,		name='resources_add'),
 ]
--- user/models.py
+++ user/models.py
@@ -28,4 +28,4 @@ class User(AbstractUser):
     objects = CustomUserManager()
 
     def __str__(self):
-        return self.name
+        return self.email
