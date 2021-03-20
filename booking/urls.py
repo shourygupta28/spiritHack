b5bfb02c97a6d0358f1868935e3a6975414a4f09
N15H4N7 <ngupta_be19@thapar.edu>
2021-03-21 01:20:17 +0530

    slot partial

---
+++ booking/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('slot/',									all_slots,				name='slots'),
+    path('slot/add',								create_slot,			name='slot_add'),
+]
